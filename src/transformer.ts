d524ee659db271cea7985e16374329eeed5f34ea
Babak Farkhoopak <44144724+babakfp@users.noreply.github.com>
2024-06-12 01:48:16 +0330

    .

--- src/transformer.ts
+++ src/transformer.ts
@@ -1,14 +1,17 @@
 import { unified } from "unified"
 import remarkParse from "remark-parse"
-import remarkRehype from "remark-rehype"
 import rehypeShiki from "@shikijs/rehype"
 import rehypeStringify from "rehype-stringify"
+
+// IMPORTANT: COMMENT THIS TO FIX THE ISSUE.
+import remarkRehype from "remark-rehype"
 
 export const transformer = async (content: string) => {
     const processor = unified()
 
     processor.use(remarkParse)
 
+    // IMPORTANT: COMMENT THIS TO FIX THE ISSUE.
     processor.use(remarkRehype)
 
     processor.use(rehypeShiki, {
